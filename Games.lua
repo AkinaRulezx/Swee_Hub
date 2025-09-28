07dab7055f0b35340fe620cab054f8a7fea47d83
R H <165597973+rhuda21@users.noreply.github.com>
2025-09-28 04:25:07 +0100

    Create Games.lua

---
+++ Games.lua
@@ -0,0 +1,5 @@
+return = {
+    [94503612388426] = "https://raw.githubusercontent.com/AkinaRulezx/beafish/refs/heads/main/Swee%20Hub",
+    [89343390950953] =
+    "https://raw.githubusercontent.com/AkinaRulezx/Swee_Hub/refs/heads/main/My%20Singing%20Brainrot.lua",
+}
